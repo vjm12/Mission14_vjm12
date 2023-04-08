e5f498354d453f61b813a97a0abdcb7f3daf4107
vjm12 <vjm12@byu.edu>
2023-04-07 21:26:21 -0600

    last

--- Startup.cs
+++ Startup.cs
@@ -12,7 +12,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace Mission14_vjm12
 {
     public class Startup
@@ -21,9 +20,7 @@ namespace Mission14_vjm12
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -34,26 +31,21 @@ namespace Mission14_vjm12
             });
             services.AddCors();
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            app.UseCors(p => p.WithOrigins("http://localhost:3000"));
-
+            app.UseCors(builder =>
+                builder.AllowAnyOrigin()
+                       .AllowAnyHeader()
+                       .AllowAnyMethod());
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
         }
     }
+
 }
